09e52a4999cbafa9845c486ad3a2e59ff898aa34
gokul3710 <gokulkrishnawaryar@gmail.com>
2022-12-07 19:04:34 +0530

    scroll block commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,21 @@
 let navOpenBtn = document.getElementById('nav-open');
 let navCloseBtn = document.getElementById('nav-close');
 let nav = document.getElementById('nav-menu');
+const body = document.body;
 
 
 navOpenBtn.addEventListener('click', ()=>{
     navOpenBtn.style.display = "none";
     navCloseBtn.style.display = "block";
     nav.style.display = "flex";
+    body.style.overflow = "hidden";
 })
 
 navCloseBtn.addEventListener('click', ()=>{
     navOpenBtn.style.display = "block";
     navCloseBtn.style.display = "none";
     nav.style.display = "none";
-
+    body.style.overflow = "scroll";
 })
+
+

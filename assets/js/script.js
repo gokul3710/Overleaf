fba4bb815891270cf98670cfb245a0b0f4982d19
gokul3710 <gokulkrishnawaryar@gmail.com>
2022-12-08 10:45:04 +0530

    js bootstrap fix commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,41 @@
-let navOpenBtn = document.getElementById('nav-open');
-let navCloseBtn = document.getElementById('nav-close');
-let nav = document.getElementById('nav-menu');
-const body = document.body;
+const features = [
+    {
+        h1: "Collaboration",
+        p: "Work together on a single version<br>View collaborator edits in real time",
+        iconClass: "fa-solid fa-users fa-4x"
+    },
+    {
+        h1: "Ease of Use",
+        p: "No complicated LaTeX installation <br> All the packages and templates you need",
+        iconClass: "fa-solid fa-thumbs-up fa-4x",
+        right: true,
+    },
+    {
+        h1: "",
+        p: `See what has been <span style="background-color: #a8d8f0;">added</span> and <span style="background-color: #e9f5fb; text-decoration: line-through">removed</span> <br> Restore to any older version`,
+        iconClass: "fa-sharp fa-solid fa-clock-rotate-left fa-4x",
+    },
+    {
+        h1: "Work from anywhere",
+        p: "Access your work from anywhere in the world <br> Work offline and sync your files via Dropbox and GitHub",
+        iconClass: "fa-solid fa-earth-americas fa-4x",
+        right:true,
+    }
+]
 
-
-navOpenBtn.addEventListener('click', ()=>{
-    navOpenBtn.style.display = "none";
-    navCloseBtn.style.display = "block";
-    nav.style.display = "flex";
-    body.style.overflow = "hidden";
+features.forEach(feature =>{
+    const featureDiv = document.createElement("div");
+    const featureDivContent = `<i class="${feature.iconClass}"></i>
+                                <div class="feature-text">
+                                    <h1>${feature.h1}</h1>
+                                    <p>${feature.p}</p>
+                                </div>
+                                `;
+    featureDiv.innerHTML = featureDivContent;
+    document.querySelector('.features-container').appendChild(featureDiv);
+    featureDiv.classList.add("feature");
+    if(feature.right){
+        featureDiv.classList.add("right");
+    }
 })
-
-navCloseBtn.addEventListener('click', ()=>{
-    navOpenBtn.style.display = "block";
-    navCloseBtn.style.display = "none";
-    nav.style.display = "none";
-    body.style.overflow = "scroll";
-})
-
 
